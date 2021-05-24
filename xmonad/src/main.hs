6bfa57d40d3093278ff93de7ed51931fd3f01847
pistachiology <im@itua.dev>
2021-05-25 02:02:24 +0700

    Init xmonad

---
+++ xmonad/src/main.hs
@@ -0,0 +1,15 @@
+
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
+
+main = do
+    xmonad $ defaults
+
+defaults = desktopConfig 
+    { terminal = "alacritty"
+    }
