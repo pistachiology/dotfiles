051b8dce31639ea5ff352ca25975e9d5043b5544
pistachiology <im@itua.dev>
2022-05-25 10:58:20 +0700

    Update direnv.fish

--- modules/fish.nix
+++ modules/fish.nix
@@ -10,8 +10,8 @@
         src = pkgs.fetchFromGitHub {
           owner = "lilyball";
           repo = "nix-env.fish";
-          rev = "00c6cc762427efe08ac0bd0d1b1d12048d3ca727";
-          sha256 = "1hrl22dd0aaszdanhvddvqz3aq40jp9zi2zn0v1hjnf7fx4bgpma";
+          rev = "7b65bd228429e852c8fdfa07601159130a818cfa";
+          sha256 = "sha256-RG/0rfhgq6aEKNZ0XwIqOaZ6K5S4+/Y5EEMnIdtfPhk=";
         };
       }
     ];
